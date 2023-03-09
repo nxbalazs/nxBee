1c9c4a975be6a8a18d96db6a54ad998cd5e171fc
Balázs Nagy <balazsnagy@Balazss-MacBook-Pro.local>
2023-03-09 22:24:31 +0100

    added reminder for a treatment

--- hive_management/utils/reports.py
+++ hive_management/utils/reports.py
@@ -1,12 +1,15 @@
 from hive_management.models import Hive, Treatment
+import datetime
 
 def check_treatment():
     hives = Hive.objects.all()
+    hdays = (datetime.datetime.today() - datetime.timedelta(days=101)).strftime('%Y-%m-%d')
     treatments = []
     for hive in hives:
-        check_treatment = Treatment.objects.filter(hive=hive)
-        if check_treatment:
+        check_treatment = Treatment.objects.filter(hive=hive).latest('created_on')
+        if check_treatment and str(check_treatment.created_on) > hdays:
             pass
         else: 
             treatments.append(hive)
+            print(hdays)
     return treatments
